1332ce9aaae2f63045103a83087113f7c23e14c6
Deepesh.Rathore <Deepesh.Rathore@mastek.com>
2016-04-28 22:52:15 +0100

    Refactoring.

--- shopping-cart/src/main/java/com/app/ShoppingCartCheckout.java
+++ shopping-cart/src/main/java/com/app/ShoppingCartCheckout.java
@@ -5,18 +5,22 @@ import java.util.List;
 
 public class ShoppingCartCheckout {
 
+    private static final BigDecimal APPLE_PRICE = new BigDecimal("0.60");
+    private static final BigDecimal ORANGE_PRICE = new BigDecimal("0.25");
+    private static final String CURRENCY_SYMBOL = "£";
+
     public String checkout(List<String> listOfItems) {
-        BigDecimal sum = new BigDecimal("0.00");
+        BigDecimal sum = BigDecimal.ZERO;
 
         for (String item : listOfItems) {
             if (item.equals("Apple")){
-                sum =  sum.add(new BigDecimal("0.60"));
+                sum =  sum.add(APPLE_PRICE);
             } else {
-                sum = sum.add(new BigDecimal("0.25"));
+                sum = sum.add(ORANGE_PRICE);
             }
         }
 
 
-        return "£"+sum.toString();
+        return CURRENCY_SYMBOL +sum.toString();
     }
 }
