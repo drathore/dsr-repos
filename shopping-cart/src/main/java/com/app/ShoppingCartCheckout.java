8e569636fef80247ca4038ddc365f98562bb340f
Deepesh.Rathore <Deepesh.Rathore@mastek.com>
2016-04-28 22:18:34 +0100

    Written a 1st failing test. Created ShoppingCartCheckout as part of this test.

---
+++ shopping-cart/src/main/java/com/app/ShoppingCartCheckout.java
@@ -0,0 +1,9 @@
+package com.app;
+
+import java.util.List;
+
+public class ShoppingCartCheckout {
+    public String checkout(List<String> listOfItems) {
+        return null;
+    }
+}
---
+++ shopping-cart/src/test/java/com/app/ShoppingCartCheckoutTest.java
@@ -0,0 +1,33 @@
+package com.app;
+
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+
+public class ShoppingCartCheckoutTest {
+
+    private ShoppingCartCheckout shoppingCartCheckout;
+
+    @Before
+    public void setup(){
+        shoppingCartCheckout = new ShoppingCartCheckout();
+    }
+
+    @Test
+    public void whenCheckoutSentAListContainingAnAppleItsPriceReturnedAsTotalPrice(){
+
+        List<String> listOfItems = Arrays.asList("Apple");
+        String totalPriceString = shoppingCartCheckout.checkout(listOfItems);
+
+        assertThat("Should have returned the price of an apple", totalPriceString, is("£0.60"));
+    }
+
+}
